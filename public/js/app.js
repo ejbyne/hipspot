5ad91fbeaad3037e69f3b817005f83964090d41c
Nick Dyer <nbdyer@gmail.com>
2015-01-23 18:21:07 +0000

    Map renders max 200 nearby drinks venues, on window load and page idle

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,47 @@
+var map;
+var service;
+var placesTypes = ['bar']; 
+
 function initialize(position) {
 
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
+  var userLatitude = position.coords.latitude;
+  var userLongitude = position.coords.longitude;
 
   var mapOptions = {
     zoom: 17,
-    center: new google.maps.LatLng(latitude, longitude),
+    center: new google.maps.LatLng(userLatitude, userLongitude),
     scaleControl: true
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  service = new google.maps.places.PlacesService(map);
 
   var currentPositionMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(latitude, longitude),
+    position: new google.maps.LatLng(userLatitude, userLongitude),
     map: map,
   });
+
+  google.maps.event.addListener(map, 'idle', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    types: placesTypes 
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, results; result = results[i]; i++) {
+      var marker = new google.maps.Marker({ 
+        map: map, 
+        position: result.geometry.location
+    });
+  }
 }
 
 function error(err) {
