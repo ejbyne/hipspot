1b7ecbf63e3f98498a7740ac149a165eed9ff24a
Mishal Islam <mishal.islam01@gmail.com>
2015-01-26 14:31:10 +0000

    adds info window when icon is clicked

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 var map;
 var service;
 var placesTypes = ['bar'];
+var infoWindow;
 
 $(function() {
   $('.tlt').textillate({ 
@@ -11,7 +12,6 @@ $(function() {
 });
 
 function initialize(position) {
-
   var userLatitude = position.coords.latitude;
   var userLongitude = position.coords.longitude;
 
@@ -21,19 +21,23 @@ function initialize(position) {
     scaleControl: true,
     styles: styles
   };
+
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   service = new google.maps.places.PlacesService(map);
+  infoWindow = new google.maps.InfoWindow();
 
   var currentPositionMarker = new google.maps.Marker({
     position: new google.maps.LatLng(userLatitude, userLongitude),
     map: map,
     icon: new google.maps.MarkerImage('img/man.svg', null, null, null, new google.maps.Size(36, 36))
   });
+
   $('#map-canvas').show();
   $('.sticky').show();
   $('.tlt').hide();
 
   google.maps.event.addListener(map, 'idle', performSearch);
+
 }
 
 function performSearch() {
@@ -69,7 +73,6 @@ function getTweetData(data) {
   heatmap = new google.maps.visualization.HeatmapLayer({ data: pointArray });
   heatmap.set('radius', 16);
   heatmap.set('opacity', 1);
-  // heatmap.set('dissipating', true);
   heatmap.setMap(map);
 }
 
@@ -79,12 +82,55 @@ function callback(results, status) {
     return;
   }
   for (var i = 0; i < results.length; i++) {
-      var marker = new google.maps.Marker({ 
-        map: map, 
-        position: results[i].geometry.location,
-        icon: new google.maps.MarkerImage('img/glass.svg', null, null, null, new google.maps.Size(24,24))
-    });
+    createMarker(results[i]);
   }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: placeLoc,
+    icon: new google.maps.MarkerImage('img/glass.svg', null, null, null, new google.maps.Size(24,24))
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+      var name = result.name + '<br>';
+
+      var address = result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
+
+      var website = function() {
+        if(result.website) {
+          var text = '<a href="' + result.website + '">' + result.website + '</a>';
+          return text;
+        } else {
+          return '';
+        }
+      };
+      var openingHours = function() {
+        if(result.opening_hours) {
+          var text = '<br>Opening Hours:<br>'
+          result.opening_hours.weekday_text.forEach(function(day) {
+            text += day + '<br>'
+          });
+          return text;
+        } else {
+          return '';
+        }
+      };
+      var details = name +
+                    address +
+                    website() +
+                    openingHours();
+      infoWindow.setContent(details);
+      infoWindow.open(map, marker)
+    })
+  });
 }
 
 function error(err) {
@@ -98,5 +144,3 @@ var options = {
 };
 
 navigator.geolocation.getCurrentPosition(initialize, error, options);
-
-
