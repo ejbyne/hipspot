c2e88e440dc93caa3f2a84d772683c1cfcd76202
ed <ejbyne@gmail.com>
2015-01-27 18:48:49 +0000

    adds search bar to view

--- public/js/app.js
+++ public/js/app.js
@@ -67,6 +67,65 @@ function initialize(position) {
 
   google.maps.event.addListener(map, 'idle', performSearch);
 
+  addSearchBox();
+
+}
+
+function addSearchBox() {
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
 }
 
 function performSearch() {
@@ -136,10 +195,10 @@ function createMarker(place) {
 
   google.maps.event.addListener(placesMarker, 'click', function() {
     service.getDetails(place, function(result, status) {
-      if (status != google.maps.places.PlacesServiceStatus.OK) {
-        alert(status);
-        return;
-      }
+      // if (status != google.maps.places.PlacesServiceStatus.OK) {
+      //   alert(status);
+      //   return;
+      // }
       var name = result.name + '<br>';
 
       var address = result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
@@ -174,7 +233,7 @@ function createMarker(place) {
 }
 
 function error(err) {
-  alert('Error(' + err.code + '): ' + err.message);
+  return false;
 }
 
 var options = {
