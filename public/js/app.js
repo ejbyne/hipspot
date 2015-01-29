31a30dfea2a185f2d9eaffbb2754d259d6a5db4c
ed <ejbyne@gmail.com>
2015-01-29 15:17:13 +0000

    created tweetSearch and performSearch js files

--- public/js/app.js
+++ public/js/app.js
@@ -69,18 +69,6 @@ var performSearch = function() {
   tweetSearch(bounds, chosenTimeSlot);
 };
 
-function placesSearch(bounds) {
-  for (var i = 0; i < placesMarkerArray.length; i++) {
-    placesMarkerArray[i].setMap(null);
-  }
-  placesMarkerArray = [];
-  var request = {
-    bounds: bounds,
-    types: chosenPlacesFilter
-  };
-  service.radarSearch(request, callback);
-}
-
 function defaultTimeSlot() {
   return Math.floor(new Date().getHours()/4) + 1;
 }
@@ -113,23 +101,6 @@ function showTweetData(data) {
   heatmap.setMap(map);
 }
 
-function callback(results, status) {
-  placesArray = results;
-  createMarkers(results, findHipSpots);
-  if (markerClusterer) {
-    markerClusterer.clearMarkers();
-  }
-  var mkOptions = {maxZoom: 16,
-                   styles: [{
-                            height: 50,
-                            url: placesImage,
-                            width: 50,
-                            textSize: 10
-                    }]
-  };
-  markerClusterer = new MarkerClusterer(map, placesMarkerArray, mkOptions);
-}
-
 function findHipSpots() {
   var hipSpots = {};
   for (var i = 0; i < placesArray.length; i++) {
@@ -155,59 +126,6 @@ function changeMarkerIcon(place) {
       placesMarkerArray[i].setIcon(new google.maps.MarkerImage('img/star-'+ chosenPlacesFilter +'.svg', null, null, null, new google.maps.Size(36,36)));
     }
   }
-}
-
-function createMarkers(results, callback) {
-  for (var i = 0; i < results.length; i++) {
-    createMarker(results[i]);
-  }
-  callback();
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var placesMarker = new google.maps.Marker({
-    placeId: place.place_id,
-    map: map,
-    position: placeLoc,
-    icon: new google.maps.MarkerImage(placesImage)
-  });
-
-  placesMarkerArray.push(placesMarker);
-
-  google.maps.event.addListener(placesMarker, 'click', function() {
-    service.getDetails(place, function(result, status) {
-      var name = result.name + '<br>';
-
-      var address = result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
-
-      var website = function() {
-        if(result.website) {
-          var text = '<a href="' + result.website + '">' + result.website + '</a>';
-          return text;
-        } else {
-          return '';
-        }
-      };
-      var openingHours = function() {
-        if(result.opening_hours) {
-          var text = '<br>Opening Hours:<br>';
-          result.opening_hours.weekday_text.forEach(function(day) {
-            text += day + '<br>';
-          });
-          return text;
-        } else {
-          return '';
-        }
-      };
-      var details = name +
-                    address +
-                    website() +
-                    openingHours();
-      infoWindow.setContent(details);
-      infoWindow.open(map, placesMarker);
-    });
-  });
 }
 
 function error(err) {
---
+++ public/js/placesSearch.js
@@ -0,0 +1,81 @@
+function placesSearch(bounds) {
+  for (var i = 0; i < placesMarkerArray.length; i++) {
+    placesMarkerArray[i].setMap(null);
+  }
+  placesMarkerArray = [];
+  var request = {
+    bounds: bounds,
+    types: chosenPlacesFilter
+  };
+  service.radarSearch(request, processPlacesSearch);
+}
+
+function processPlacesSearch(results, status) {
+  placesArray = results;
+  createMarkers(results, findHipSpots);
+  if (markerClusterer) {
+    markerClusterer.clearMarkers();
+  }
+  var mkOptions = {maxZoom: 16,
+                   styles: [{
+                            height: 50,
+                            url: placesImage,
+                            width: 50,
+                            textSize: 10
+                    }]
+  };
+  markerClusterer = new MarkerClusterer(map, placesMarkerArray, mkOptions);
+}
+
+function createMarkers(results, callback) {
+  for (var i = 0; i < results.length; i++) {
+    createMarker(results[i]);
+  }
+  callback();
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var placesMarker = new google.maps.Marker({
+    placeId: place.place_id,
+    map: map,
+    position: placeLoc,
+    icon: new google.maps.MarkerImage(placesImage)
+  });
+
+  placesMarkerArray.push(placesMarker);
+
+  google.maps.event.addListener(placesMarker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      var name = result.name + '<br>';
+
+      var address = result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
+
+      var website = function() {
+        if(result.website) {
+          var text = '<a href="' + result.website + '">' + result.website + '</a>';
+          return text;
+        } else {
+          return '';
+        }
+      };
+      var openingHours = function() {
+        if(result.opening_hours) {
+          var text = '<br>Opening Hours:<br>';
+          result.opening_hours.weekday_text.forEach(function(day) {
+            text += day + '<br>';
+          });
+          return text;
+        } else {
+          return '';
+        }
+      };
+      var details = name +
+                    address +
+                    website() +
+                    openingHours();
+      infoWindow.setContent(details);
+      infoWindow.open(map, placesMarker);
+    });
+  });
+}
