c1340e02c3e718cc25a0d932d4a491cc3da2e3f3
Nick Dyer <nbdyer@gmail.com>
2015-01-29 14:49:34 +0000

    Refactors app.js

---
+++ public/js/jqueryEvents.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  $('.tlt').textillate({
+    in: { effect: 'splat', delay: 20 },
+    out: { effect: 'bounceOut', delay: 20 },
+    loop: true,
+  });
+
+  $('.button-group').find("[data-pick='" + defaultTimeSlot() + "']").css("background-color", "#007095");
+
+  $('.button').on('click', function(event) {
+    event.preventDefault();
+    $(this).parent().siblings().find('a').css("background-color", "#00aced");
+    $(this).css("background-color", "#007095");
+    chosenTimeSlot = $(this).data('pick');
+    tweetSearch(map.getBounds(), chosenTimeSlot);
+  });
+
+  $('.placesFilter').on('click', function(event) {
+    event.preventDefault();
+    chosenPlacesFilter = [$(this).data('filter')];
+    placesImage = "img/" + $(this).data('filter') + ".svg";
+    placesSearch(map.getBounds());
+  });
+
+
+});
