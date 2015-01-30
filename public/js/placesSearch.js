7b5e4a639aaa02dfd2b3f5a26b65c08242da356f
ed <ejbyne@gmail.com>
2015-01-30 11:28:45 +0000

    refactors placesSearch.js

--- public/js/placesSearch.js
+++ public/js/placesSearch.js
@@ -1,4 +1,4 @@
-function placesSearch(bounds) {
+var placesSearch = function(bounds) {
   for (var i = 0; i < placesMarkerArray.length; i++) {
     placesMarkerArray[i].setMap(null);
   }
@@ -10,31 +10,31 @@ function placesSearch(bounds) {
   service.radarSearch(request, processPlacesSearch);
 }
 
-function processPlacesSearch(results, status) {
+var processPlacesSearch = function(results, status) {
   placesArray = results;
   createMarkers(results, findHipSpots);
   if (markerClusterer) {
     markerClusterer.clearMarkers();
   }
   var mkOptions = {maxZoom: 16,
-                   styles: [{
+                  styles: [{
                             height: 50,
                             url: placesImage,
                             width: 50,
                             textSize: 10
-                    }]
+                          }]
   };
   markerClusterer = new MarkerClusterer(map, placesMarkerArray, mkOptions);
 }
 
-function createMarkers(results, callback) {
+var createMarkers = function(results, callback) {
   for (var i = 0; i < results.length; i++) {
     createMarker(results[i]);
   }
   callback();
 }
 
-function createMarker(place) {
+var createMarker = function(place) {
   var placeLoc = place.geometry.location;
   var placesMarker = new google.maps.Marker({
     placeId: place.place_id,
@@ -47,40 +47,46 @@ function createMarker(place) {
 
   google.maps.event.addListener(placesMarker, 'click', function() {
     service.getDetails(place, function(result, status) {
-      var closeModal = '<div id="closeModal">✕</div>';
-
-      var name = '<strong>' + result.name + '</strong><br>';
-
-      var address = result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
-
-      var website = function() {
-        if(result.website) {
-          var text = '<a href="' + result.website + '">' + result.website + '</a>';
-          return text;
-        } else {
-          return '';
-        }
-      };
-      var openingHours = function() {
-        if(result.opening_hours) {
-          var text = '<br><strong>Opening Hours:</strong><br>';
-          result.opening_hours.weekday_text.forEach(function(day) {
-            text += day + '<br>';
-          });
-          return text;
-        } else {
-          return '';
-        }
-      };
       var details = closeModal +
-                    name +
-                    address +
-                    website() +
-                    openingHours();
+                    placeName(result) +
+                    placeAddress(result) +
+                    placeWebsite(result) +
+                    placeOpeningHours(result);
       $('#infoModal').html(details).show();
       $('#closeModal').on('click', function() {
         $('#infoModal').hide();
       });
     });
   });
-}
+};
+
+var closeModal = '<div id="closeModal">✕</div>';
+
+var placeName = function(result) {
+  return '<strong>' + result.name + '</strong><br>';
+};
+
+var placeAddress = function(result) {
+  return result.vicinity + ' ' + result.address_components[result.address_components.length-1].long_name + '<br>';
+};
+
+var placeWebsite = function(result) {
+  if(result.website) {
+    var text = '<a href="' + result.website + '">' + result.website + '</a>';
+    return text;
+  } else {
+    return '';
+  }
+};
+
+var placeOpeningHours = function(result) {
+  if(result.opening_hours) {
+    var text = '<br><strong>Opening Hours:</strong><br>';
+    result.opening_hours.weekday_text.forEach(function(day) {
+      text += day + '<br>';
+    });
+    return text;
+  } else {
+    return '';
+  }
+};
